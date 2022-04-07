7896b2b6ffb105d1a9764d0c427fe15b2bda3a85
arthurariza <tucaariza@gmail.com>
2022-04-06 23:17:30 -0300

    feat: root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  root 'main#index'
   get 'about', to: 'about#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
