b202cf46f5c135c7cd1fbd05278e8b1b92364fbc
arthurariza <tucaariza@gmail.com>
2022-04-08 23:16:37 -0300

    feat: user log_in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,27 @@
-class SessionController < ApplicationController
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: user_params[:email])
+
+    if @user.present? && @user.authenticate(user_params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Successfully Logged In"
+    else
+      flash[:alert] = "Your Credentials Could Not Be Validated"
+      render :new
+    end
+  end
+
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: "Logged Out"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@
 Rails.application.routes.draw do
   root 'main#index'
 
+  # Sign Up
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  # Sign In
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
 
   get 'about', to: 'about#index'
